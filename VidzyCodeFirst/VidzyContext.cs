aeb980596b0d66450edaf0eadc384957562165bf
Jose Useche <jose11useche@hotmail.com>
2019-08-12 17:08:39 -0400

    VidzyCodeFirst

---
+++ VidzyCodeFirst/VidzyContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using VidzyCodeFirst.EntityConfigurations;
+
+namespace VidzyCodeFirst
+{
+    public class VidzyContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new VideoConfiguration());
+            modelBuilder.Configurations.Add(new GenreConfiguration());
+        }
+    }
+}
